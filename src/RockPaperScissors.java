21425e0e96a7782437856e83ff9d454de4181dae
James Haller <JamesHaller27@gmail.com>
2017-07-25 23:27:25 -0400

    Removed code for exercise

--- src/RockPaperScissors.java
+++ src/RockPaperScissors.java
@@ -10,31 +10,17 @@ public class RockPaperScissors {
         String computerChoice;
 
         do {
+	// Your code here 
+        // --------------------------------------------------------------------
+
             // Computer makes a choice
-            //---------------------------------------------------------------------
             int choiceNumber = (int) (Math.random() * 3);  // Randomly picks a number between 0-2 (inclusive)
-            if (choiceNumber == 0) {
-                computerChoice = "Rock";
-            } else if (choiceNumber == 1) {
-                computerChoice = "Paper";
-            } else if (choiceNumber == 2) {
-                computerChoice = "Scissors";
-            } else {
-                computerChoice = "WAT";
-            }
 
             // Human makes a choice
-            //---------------------------------------------------------------------
-            // Print menu
             System.out.println("Enter your choice: Rock, Paper, Scissors, or Exit");
             humanChoice = scanner.next();
 
-            // Print results
-            //---------------------------------------------------------------------
-            System.out.println("Human:     " + humanChoice);
-            System.out.println("Computer:  " + computerChoice);
-            System.out.println();
-
+        //---------------------------------------------------------------------
         } while(!humanChoice.toLowerCase().equals("exit"));
 
         System.out.println("Good bye!");
